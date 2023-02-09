a489d6b83769bf0f24699916b0f57a4e0fd0253c
NCStorey <ncsansom@hotmail.co.uk>
2023-02-09 16:10:39 +0000

    pixabay api now works

--- assets/js/Pexels_test.js
+++ assets/js/Pexels_test.js
@@ -1,1 +1,12 @@
-let PixaBaseBayURL = 'https://pixabay.com/api/?key=33509086-cc05bf73b92e15cea747beecb&q=yellow+flowers&image_type=photo'
+let searchQuery = 'Russia'
+let PixaBaseBayURL = 'https://pixabay.com/api/?key=33509086-cc05bf73b92e15cea747beecb&q='+ searchQuery +'&image_type=photo&category=travel&safesearch=true&per_page=5'
+
+
+$.ajax({
+    url: PixaBaseBayURL,
+    method: "GET",
+    success: (function(response){
+        console.log(response)
+    })
+
+})
