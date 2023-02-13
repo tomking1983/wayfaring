761f4154e4be33bd6fe0db76892760d0416f7b5f
NCStorey <ncsansom@hotmail.co.uk>
2023-02-13 07:54:42 +0000

    working on fav places

--- assets/js/Nathalie-Script.js
+++ assets/js/Nathalie-Script.js
@@ -1,6 +1,15 @@
-let searchQuery = 'Russia'
-let PixaBaseBayURL = 'https://pixabay.com/api/?key=33509086-cc05bf73b92e15cea747beecb&q='+ searchQuery +'&image_type=photo&category=travel&safesearch=true&per_page=5'
-let testPixabay = $('#test-pixabay')
+let searchQuery = 'Russia';
+let PixaBaseBayURL = 'https://pixabay.com/api/?key=33509086-cc05bf73b92e15cea747beecb&q='+ searchQuery +'&image_type=photo&category=travel&safesearch=true&per_page=5';
+let testPixabay = $('#test-pixabay');
+
+let favPlacesCont = $('#fav_places');
+
+
+
+
+
+
+
 
 
 // $.ajax({
