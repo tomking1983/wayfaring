66e2a0ac212a7cbe2305bb900419ca5eee9f11f6
NCStorey <ncsansom@hotmail.co.uk>
2023-02-14 14:54:13 +0000

    html for pixabay feature

--- assets/js/pixabay.js
+++ assets/js/pixabay.js
@@ -1,22 +1,15 @@
-let searchQuery = 'Russia'
-let PixaBaseBayURL = 'https://pixabay.com/api/?key=33509086-cc05bf73b92e15cea747beecb&q='+ searchQuery +'&image_type=photo&category=travel&safesearch=true&per_page=5'
-let testPixabay = $('#test-pixabay')
 
 
-// $.ajax({
-//     url: PixaBaseBayURL,
-//     method: "GET",
-//     success: (function(response){
 
-//         console.log(response)
-//         console.log(response.hits[1].largeImageURL)
+document.getElementById("submit").addEventListener("click", function () {
 
-//         let imgURL = response.hits[1].largeImageURL ;
-//         let travelimg = $('<img>');
-//         travelimg.attr('src', imgURL);
-//         testPixabay.append(travelimg);
+fetch('https://pixabay.com/api/?key=33509086-cc05bf73b92e15cea747beecb&q='+ countryName +'&image_type=photo&category=travel&safesearch=true&per_page=9')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
 
-//     })
-// })
-
+})
 
